26a4de84deb3225d744a97906ddd83521be1a1c6
Peter <Mukatome@Peters-MacBook-Pro.local>
2016-08-15 12:50:55 -0600

    add main.ino

---
+++ main.ino
@@ -0,0 +1,13 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "SparkFunBME280/SparkFunBME280.h"
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("BzBox v0.1.0")
+
+}
+
+void loop() {
+
+
+}
